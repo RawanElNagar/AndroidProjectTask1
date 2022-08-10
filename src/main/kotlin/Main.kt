1243a8d547d5c87ebe0e9de9027abf1dcbd4dce3
DELL <rawnanelnagar456@gmail.com>
2022-08-10 22:02:05 +0200

    this version 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main() {
     var number1 :Double = 20.0
      number1 = 12.5
-    var number2= 20.5
+    var number2= 20
+    number2= 11
     var results= number1+number2
     println(results)
     // first update
